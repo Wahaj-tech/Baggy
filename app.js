f36be87ec4ba70fb173aa1127f91d76110175498
khanwahaj016@gmail.com <user.email like: "khanwahaj016@gmail.com>
2025-12-04 00:21:32 +0530

    basic setup

---
+++ app.js
@@ -0,0 +1,18 @@
+//npm init -y
+//npm i express mongoose ejs bcrypt jsonwebtoken cookie-parser multer
+
+const express=require("express")
+const app=express()
+const cookieParser=require('cookie-parser')
+const path=require('path')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.send('hey')
+})
+app.listen(3000);
